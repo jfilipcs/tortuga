be1a42c6e8da0389dc3fc541985fb04c91ca9858
Estudiante <Estudiante@10.203.3.4>
2017-02-21 15:34:54 -0500

    Modificado

---
+++ src/ejerciciopoo/POO.java
@@ -0,0 +1,30 @@
+package ejerciciopoo;
+
+import ch.aplu.turtle.Turtle;
+
+/**
+ *
+ * @author Equipo
+ */
+public class POO {
+   private  Turtle joe;
+   
+   public POO(){
+     this.joe =  new Turtle(); 
+   }
+   
+   public void dibujar(){
+       for(int i=0;i<4;i++){
+      joe.fd(20);
+      joe.rt(90);
+   } 
+   }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       POO e = new POO();
+       e.dibujar();
+    }
+    
+}
