8a7d13a1844ebb9ae2cf66d5db17524c8c27c06a
Estudiante <Estudiante@10.203.3.4>
2017-02-21 15:42:31 -0500

    Modificado

--- src/ejerciciopoo/POO.java
+++ src/ejerciciopoo/POO.java
@@ -13,18 +13,41 @@ public class POO {
      this.joe =  new Turtle(); 
    }
    
-   public void cuadrado(){
+   public void triangulo(double a){
+       for(int i=0;i<3;i++){
+      joe.fd(a);
+      joe.rt(120);
+   } 
+   
+   
+   public void cuadrado(double a){
        for(int i=0;i<4;i++){
-      joe.fd(20);
+      joe.fd(a);
       joe.rt(90);
    } 
    }
+   
+   public void pentagono(double a){
+       for(int i=0;i<4;i++){
+      joe.fd(a);
+      joe.rt(72);
+   } 
+   }
+   
+   public void hexa(double a){
+       for(int i=0;i<4;i++){
+      joe.fd(a);
+      joe.rt(60);
+   } 
+   }
+   
+   
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
        POO e = new POO();
-       
+       e.cuadrado(120);
     }
     
 }
